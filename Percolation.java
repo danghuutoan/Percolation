19499510346c8e2fb4d825be0dc0ac399347c652
danghuutoan <danghuutoan2002@gmail.com>
2017-05-28 19:02:14 +0700

    add constructor code

--- Percolation.java
+++ Percolation.java
@@ -3,8 +3,17 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int[][] grid;
+    public static final int SITE_BLOCKED = 0;
+    public static final int SITE_OPEN = 0;
     // create n-by-n grid, with all sites blocked
     public Percolation(int n){ 
+        grid = new int[n][n];
+        for (int row = 0; row < n; row++) {
+            for(int col = 0; col < n; col++){
+                grid[row][col] = SITE_BLOCKED;
+            }
+        }
     }
     // open site (row, col) if it is not open already
     public void open(int row, int col){
